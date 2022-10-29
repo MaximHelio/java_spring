f811c20c981fff7d9e27f83f7b35efbec5f8d329
kknni <gasiorowicz10@gmail.com>
2022-10-29 19:55:23 +0900

    Add 20221029

--- java-express/src/JDBC/Board/DAO/BoardDAO.java
+++ java-express/src/JDBC/Board/DAO/BoardDAO.java
@@ -1,10 +1,80 @@
 package JDBC.Board.DAO;
 
+import java.util.LinkedList;
+import java.util.List;
+
 import JDBC.JDBConnection;
 import JDBC.Board.DTO.Board;
 
 public class BoardDAO extends JDBConnection{
 
+	// 데이터 목록 조회 배열: 정적 자료구조, 리스트: 동적 자료구조=> 추가 삭제 가능, 객체만 담을 수 있음, int 및 double 못담음
+	public List<Board> selectList() {
+		
+		LinkedList<Board> boardList = new LinkedList<Board>();
+		
+		String sql = " SELECT *"
+				   + " FROM board";
+		
+		try {
+			stmt = con.createStatement(); 	//쿼리문 생성
+			rs = stmt.executeQuery(sql);	//쿼리 실행
+			
+			while( rs.next() ) {
+				Board board = new Board();
+				
+				board.setBoard_no(rs.getInt("board_no")); //받아온 첫번째 커서의 board_no라는 컬럼, 현재 커서에 있는 컬럼의 값을 가져옴
+				board.setTitle(rs.getString("title"));
+				board.setContent(rs.getString("content"));
+				board.setWriter(rs.getString("writer"));
+				board.setReg_date(rs.getDate("reg_date"));
+				board.setUpd_date(rs.getDate("upd_date"));
+				
+				boardList.add(board);
+			}
+			
+		} catch (Exception e) {
+			System.out.println("게시글 목록 조회 시, 예외 발생");
+			e.printStackTrace();
+		}
+		return boardList; //  반환해야 끝남
+	}
+	
+	// 데이터 조회
+	// 데이터 목록 조회 배열: 정적 자료구조, 리스트: 동적 자료구조=> 추가 삭제 가능, 객체만 담을 수 있음, int 및 double 못담음
+	public Board select(int board_no) {
+		
+		Board board = new Board();
+		String sql = " SELECT * " 
+				   + " FROM board"
+				   + " WHERE board_no = ? "; // 물음표 넘겨주는 쿼리는 preparestatment 메서드로 만듦
+		
+		try {
+			psmt = con.prepareStatement(sql); 	//쿼리문 생성
+			psmt.setInt(1, board_no);			// 파라미터 매핑
+			rs = psmt.executeQuery();		//쿼리 실행
+			
+			// 다음 커서가 존재했다면
+			if( rs.next() ) {
+				
+				board.setBoard_no(rs.getInt("board_no")); //받아온 첫번째 커서의 board_no라는 컬럼, 현재 커서에 있는 컬럼의 값을 가져옴
+				board.setTitle(rs.getString("title"));
+				board.setContent(rs.getString("content"));
+				board.setWriter(rs.getString("writer"));
+				board.setReg_date(rs.getDate("reg_date"));
+				board.setUpd_date(rs.getDate("upd_date"));
+				
+			} else {
+				board = null;
+			}
+			
+		} catch (Exception e) {
+			System.out.println("게시글 조회 시, 예외 발생");
+			e.printStackTrace();
+		}
+		return board; //  반환해야 끝남
+	}
+	
 	// 데이터 등록
 	public int insert(Board board) {
 		
@@ -13,18 +83,65 @@ public class BoardDAO extends JDBConnection{
 		String sql = " INSERT INTO board ( title, content, writer ) "
 					+ " VALUES(?, ?, ?)";
 		try {
-			
 			psmt = con.prepareStatement(sql); 		// 쿼리문 생성
 			psmt.setString(1, board.getTitle());
 			psmt.setString(2, board.getContent());
 			psmt.setString(3, board.getWriter());
-			result = psmt.executeUpdate();		// DML 쿼리 실행: 적용된 행의 수 반환
-			
+			result = psmt.executeUpdate();		// DML 쿼리 실행: 적용된 행의 개수 반환
 		} catch (Exception e) {
 			System.out.println("게시글 등록시, 예외발생");
 			e.printStackTrace();
 		}
 		return result;
 	}
+	
+	// 데이터 수정
+	public int update(Board board) {
+		
+		int result = 0;
+		
+		String sql = " UPDATE board"
+					+ " SET title = ? "
+					+ "    ,content = ? "
+					+ "	   ,writer = ?"
+					+" WHERE board_no = ? ";
+		try {
+			psmt = con.prepareStatement(sql); 		// 쿼리문 생성
+			psmt.setString(1, board.getTitle());
+			psmt.setString(2, board.getContent());
+			psmt.setString(3, board.getWriter());
+			psmt.setInt(4, board.getBoard_no());
+			result = psmt.executeUpdate();		// DML 쿼리 실행: 적용된 행의 수 반환
+		
+		} catch (Exception e) {
+			System.out.println("게시글 등록시, 예외발생");
+			e.printStackTrace();
+		}
+		return result;
+	}
+	
+	
+	// 데이터 삭제
+	public int delete(int board_no) {
+		
+		int result = 0;
+		
+		String sql = " DELETE "
+					+ " FROM board "
+					+" WHERE board_no = ? ";
+		try {
+			psmt = con.prepareStatement(sql); 		// 쿼리문 생성
+
+			psmt.setInt(1, board_no);
+			result = psmt.executeUpdate();		// DML 쿼리 실행: 적용된 행의 수 반환
+		
+		} catch (Exception e) {
+			System.out.println("게시글 삭제시, 예외발생");
+			e.printStackTrace();
+		}
+		return result;
+	}
+	
+	
 	
 }
--- java-express/src/JDBC/Board/Main.java
+++ java-express/src/JDBC/Board/Main.java
@@ -1,5 +1,6 @@
 package JDBC.Board;
 
+import java.util.List;
 import java.util.Scanner;
 
 //import JDBC.JDBConnection;
@@ -8,6 +9,7 @@ import JDBC.Board.DTO.Board;
 
 public class Main {
 	
+	static List<Board> boardList;
 	static Board board;
 	static BoardDAO dao = new BoardDAO();
 	static Scanner sc = new Scanner(System.in);
@@ -35,8 +37,95 @@ public class Main {
 		String content = sc.nextLine();
 		
 		board = new Board(title, content, writer);
-		dao.insert(board);
+		int result = dao.insert(board);
+		
+		if(result >0) {
+			System.out.println(result + " 건이 등록되었습니다.");
+		} else {
+			System.out.println("등록이 실패하였습니다.");
+		}
 	
+	}
+	
+	public static void menuUpdate() {
+		System.out.println("******** [글수정] *******");
+		
+		System.out.println("게시글 번호: ");
+		int board_no = sc.nextInt();
+		sc.nextLine();
+		
+		System.out.println("제목: ");
+		String title = sc.nextLine();
+		
+		System.out.println("작성자: ");
+		String writer  = sc.nextLine();
+		
+		System.out.println("내용: ");
+		String content = sc.nextLine();
+		
+		board = new Board(title, content, writer);
+		board.setBoard_no(board_no);
+		int result = dao.update(board);
+		
+		if(result >0) {
+			System.out.println(result + " 건이 수정되었습니다.");
+		} else {
+			System.out.println("수정이 실패하였습니다.");
+		}
+		
+	}
+	
+	public static void menuDelete() {
+		System.out.println("******** [글삭제] ********");
+		
+		System.out.println("게시글 번호: ");
+		int board_no = sc.nextInt();
+		sc.nextLine();
+		
+		int result = dao.delete(board_no);
+		if(result >0) {
+			System.out.println(result + " 건이 삭제되었습니다.");
+		} else {
+			System.out.println("삭제가 실패하였습니다.");
+		}
+	}
+	
+	public static void menuList() {
+		System.out.println("****** [전체목록] ******");
+		
+		boardList = dao.selectList();
+		printAll();
+		
+	}
+	
+	public static void menuSelect() {
+		System.out.println("****** [조회하기] *******");
+		
+		System.out.println("게시글 번호 : ");
+		int board_no = sc.nextInt();
+		sc.nextLine();
+		
+		board = dao.select(board_no);
+		if (board == null) {
+			System.out.println(board_no + "번 게시글이 존재하지 않습니다.");
+			return;
+		}else {			
+			print(board);
+		}
+		
+	}
+	
+	public static void printAll() {
+		for (Board board : boardList) {
+			print(board);
+		}
+	}
+	
+	public static void print(Board board) {
+		System.out.println("[게시글 번호:" + board.getBoard_no() + "]");
+		System.out.println("- 제목: " + board.getTitle());
+		System.out.println("- 작성자: " + board.getWriter());
+		System.out.println("- 내용: " + board.getContent());
 	}
 	
 	public static void main(String[] args) {
@@ -54,12 +143,19 @@ public class Main {
 			}
 			
 			switch (menuNo) {
-			case 1:
-				menuWrite();
-				break;
-			default:
-				System.out.println("(0~5) 사이의 숫자를 입력하세요! ");
-				break;
+				case 1:menuWrite();
+					break;
+				case 2:menuUpdate();
+					break;
+				case 3:menuDelete();
+					break;
+				case 4:menuSelect();
+					break;
+				case 5:menuList();
+					break;
+				default:
+					System.out.println("(0~5) 사이의 숫자를 입력하세요!");
+					break;
 			}
 					
 		} while(true);
